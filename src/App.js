0d5fd48eb342efc542a9b327da8975b7f39333c2
Vipul <vipuldeep95@gmail.com>
2020-08-30 21:57:54 +1000

    fetch() and if else

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+// Ajax request using fetch API
+// javascript ES6 and AJAX have promises to tell that when they will get the result they will surely let us know
+// promise is a object that may produce a single value in future
+//3 states: full-filled, rejected and pending
+export class App extends Component {
+  state = {
+    loading: true,
+    person: null
+  };
+
+  async componentDidMount() {
+    const url = "https://api.randomuser.me/";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.results[0], loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>Please be patient! loading data for you!</div>;
+    }
+
+    if (!this.state.person) {
+      return <div>Sorry, please try again, We didn't get a person!</div>;
+    }
+
+    return (
+      <div>
+        <div class='tc'>{this.state.person.name.title}</div>
+        <div>{this.state.person.name.first}</div>
+        <div>{this.state.person.name.last}</div>
+        <img alt='image' src={this.state.person.picture.large} />
+      </div>
+    );
+  }
+}
 export default App;
